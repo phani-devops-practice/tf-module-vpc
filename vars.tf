15a70695d82241d843210294bd1f0f2a0f101f2e
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-30 08:30:34 +0530

    updating ./terraform/tf-module-vpc

---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "ENV" {}
+variable "PROJECT" {}
+variable "VPC_CIDR" {}
+variable "PUBLIC_SUBNETS_CIDR" {}
+variable "PRIVATE_SUBNETS_CIDR" {}
+variable "AZ" {}
+variable "DEFAULT_VPC_ID" {}
+variable "DEFAULT_VPC_CIDR" {}
+variable "DEFAULT_VPC_RT" {}
+
